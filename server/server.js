ef212a906c7e4a02330d30d1a7819b265c16f5bf
= <=>
2021-07-04 23:00:36 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -28,6 +28,14 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
